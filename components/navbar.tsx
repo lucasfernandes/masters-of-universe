8c50615be84cd6df8adf2a05d855982455248e3e
lucasfernandes <lucas.fernandes.silveira@gmail.com>
2022-01-02 18:53:28 -0300

    fix github link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <div className=" py-4 rounded-lg flex flex-row justify-between items-center">
       <Image src={Logo} width="300" height="116" alt="logo" className='drop-shadow-xl opacity-90' />
       <div className="text-xl bg-gray-600 flex flex-row justify-center items-center space-x-4 px-8 py-2 pt-3 rounded-full bg-opacity-80">
-        <a href="https://twitter.com/lfsilveira" target="blank">
+        <a href="https://github.com/lucasfernandes" target="blank">
           <Image src={Github} width="34" height="34" alt="twitter" className='drop-shadow-xl' />
         </a>
         <a href="https://twitter.com/lfsilveira" target="blank">
